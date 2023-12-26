087390ba646f42ce6b058f06f9d81d2a3a77baa1
sstej88 <sunkara.sai@northeastern.edu>
2023-12-25 20:05:12 -0500

    Cookie Check

--- routes/users.js
+++ routes/users.js
@@ -13,12 +13,23 @@ dotenv.config();
 let isLoggedIn = (req, res, next) => {
   logger.info("METHOD: Is Logged In Check");
   statsdClient.increment("api.calls.method.CHECK_LOGIN_STATUS");
-  let userData = JSON.parse(req.cookies.userProfile);
-  if (req.cookies.userProfile && userData.email_verified) {
+
+  if (req.cookies.userProfile) {
+    let userData = JSON.parse(req.cookies.userProfile);
+    if (userData.email_verified) {
+      logger.info("METHOD: Is Logged In Check Successful");
+      statsdClient.increment("api.calls.method.LOGIN_STATUS_SUCCESSFUL");
+      next();
+    } else {
+      logger.info("METHOD: Is Logged In Email Verification Failed");
+      statsdClient.increment(
+        "api.calls.method.LOGIN_STATUS_FAILED_DUE_TO_EMAIL_VERIFICATION"
+      );
+      res.cookie("userProfile", "", { expires: new Date(0), httpOnly: true });
+      res.cookie("connect.sid", "", { expires: new Date(0), httpOnly: true });
+      res.redirect(constants.redirectionAfterLogout);
+    }
     // req.session.userProfile
-    logger.info("METHOD: Is Logged In Check Successful");
-    statsdClient.increment("api.calls.method.LOGIN_STATUS_SUCCESSFUL");
-    next();
   } else {
     logger.info("METHOD: Is Logged In Check Failed");
     statsdClient.increment("api.calls.method.LOGIN_STATUS_FAILED");
