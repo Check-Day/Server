673709b436de51bdfe4a3cd2a9da25cb38257962
sstej88 <sunkara.sai@northeastern.edu>
2023-12-30 16:19:12 -0500

    to check

--- packer/runner.sh
+++ packer/runner.sh
@@ -20,25 +20,22 @@ FLUSH PRIVILEGES;
 SQL
 sudo systemctl start mysql.service
 
-sudo cat <<EOF | sudo tee /etc/systemd/system/server.service
-[Unit]
-Description=index.js
-Documentation=https://checkday.app/
-Wants=network-online.target
-After=network-online.target cloud-final.service
-
-[Service]
-EnvironmentFile=/home/ubuntu/server/.env
-Type=simple
-User=ubuntu
-WorkingDirectory=/home/ubuntu/server
-ExecStart=/usr/bin/node /home/ubuntu/server/index.js
-Restart=on-failure
-
-[Install]
-WantedBy=cloud-init.target
-EOF
-
-sudo systemctl daemon-reload
-sudo systemctl enable server.service
-sudo systemctl start server.service
+# sudo cat <<EOF | sudo tee /etc/systemd/system/server.service
+# [Unit]
+# Description=index.js
+# Documentation=https://checkday.app/
+# Wants=network-online.target
+# After=network-online.target cloud-final.service
+# [Service]
+# EnvironmentFile=/home/ubuntu/server/.env
+# Type=simple
+# User=ubuntu
+# WorkingDirectory=/home/ubuntu/server
+# ExecStart=/usr/bin/node /home/ubuntu/server/index.js
+# Restart=on-failure
+# [Install]
+# WantedBy=cloud-init.target
+# EOF
+# sudo systemctl daemon-reload
+# sudo systemctl enable server.service
+# sudo systemctl start server.service
