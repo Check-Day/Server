bc1e462ad04e0174ae40cd3983a3e0386a1235c2
sstej88 <sunkara.sai@northeastern.edu>
2023-12-30 18:30:07 -0500

    updated runner

--- packer/runner.sh
+++ packer/runner.sh
@@ -17,22 +17,22 @@ ALTER USER '$DATABASE_USERNAME'@'$DATABASE_HOST' IDENTIFIED WITH mysql_native_pa
 FLUSH PRIVILEGES;
 SQL
 sudo systemctl start mysql.service
-# sudo cat <<EOF | sudo tee /etc/systemd/system/checkday.service
-# [Unit]
-# Description=index.js
-# Documentation=https://checkday.app/
-# Wants=network-online.target
-# After=network-online.target cloud-final.service
-# [Service]
-# EnvironmentFile=/home/ubuntu/.env
-# Type=simple
-# User=ubuntu
-# WorkingDirectory=/home/ubuntu
-# ExecStart=/usr/bin/node /home/ubuntu/index.js
-# Restart=on-failure
-# [Install]
-# WantedBy=cloud-init.target
-# EOF
-# sudo systemctl daemon-reload
+sudo cat <<EOF | sudo tee /etc/systemd/system/checkday.service
+[Unit]
+Description=index.js
+Documentation=https://checkday.app/
+Wants=network-online.target
+After=network-online.target cloud-final.service
+[Service]
+EnvironmentFile=/home/ubuntu/.env
+Type=simple
+User=ubuntu
+WorkingDirectory=/home/ubuntu
+ExecStart=/usr/bin/node /home/ubuntu/index.js
+Restart=on-failure
+[Install]
+WantedBy=cloud-init.target
+EOF
+sudo systemctl daemon-reload
 # sudo systemctl enable checkday.service
 # sudo systemctl start checkday.service
